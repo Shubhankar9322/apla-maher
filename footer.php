15a9bebbfb5c3a094cedfce58f38734f7f20c8be
Shubhankar9322 <109858997+Shubhankar9322@users.noreply.github.com>
2023-09-13 11:19:56 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<div class="container-fluid bg-dark footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-primary mb-4">Our Office</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>Flat No E-1203,Majestic Venice Dhayri(Part)(N.V.) 411041</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt text-primary me-3"></i>+917498015110</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>aaplmaher@gmail.com</p>
+                    <div class="d-flex pt-3">
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-square btn-primary rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-primary mb-4">Quick Links</h4>
+                    <a class="btn btn-link" href="">About Us</a>
+                    <a class="btn btn-link" href="">Contact Us</a>
+                    <a class="btn btn-link" href="">Our Services</a>
+                   
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+    <!-- Copyright Start -->
+    <div class="container-fluid copyright py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a class="fw-medium" href="#">AAPL MAHER</a>, All Right Reserved.
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                   
+                    Designed  <a class="fw-medium" href="">&</a> Distributed By <a class="fw-medium" href="">Nam</a>
+                </div>
+            </div>
+        </div>
+    </div>
